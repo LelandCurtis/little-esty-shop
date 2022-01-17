fe6c2f65409757b47b0caeab0f6c66c7c79d5b34
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-17 10:09:51 -0700

    Add validation test to discount model.

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
-  it { should belong_to(:merchant) }
-  it { should have_many(:items).through(:merchant) }
-  it { should have_many(:invoice_items).through(:items) }
-  it { should have_many(:invoices).through(:invoice_items) }
-  it { should have_many(:customers).through(:invoices) }
-  it { should have_many(:transactions).through(:invoices) }
+
+  describe 'associations' do
+    it { should belong_to(:merchant) }
+    it { should have_many(:items).through(:merchant) }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:quantity)}
+    it { should validate_numericality_of(:quantity).is_greater_than(0)}
+    it { should validate_presence_of(:discount)}
+    it { should validate_numericality_of(:discount).is_greater_than(0)}
+  end
 end
