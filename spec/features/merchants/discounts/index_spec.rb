2c1a7f379728ad9646da8e9029df2e596ebf83bd
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-16 23:22:54 -0700

    fix typo.

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'merchant_discount index page' do
     discount_2 = create(:discount, merchant: merchant, quantity: 12, discount: 15)
 
     visit "/merchants/#{merchant.id}/discounts"
-    
+
     within "div.discount_#{discount_1.id}" do
       expect(page).to have_content("Discount ##{discount_1.id}")
       expect(page).to have_content("Percent Discount: 10%")
@@ -19,6 +19,5 @@ RSpec.describe 'merchant_discount index page' do
       expect(page).to have_content("Percent Discount: 15%")
       expect(page).to have_content("Quantity Threshold: 12")
     end
-
   end
 end
