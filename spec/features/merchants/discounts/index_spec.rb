37cd3dcdeb2819a6dbe9939e1944615ef22c9856
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-17 11:52:31 -0700

    Add test for index link to show page.

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'merchant_discount index page' do
   it "lists all discounts, including thier percentage discounts and quantity thresholds" do
     merchant = create(:merchant)
     visit "/merchants/#{merchant.id}/discounts"
-    
+
     discount_1 = create(:discount, merchant: merchant, quantity: 10, discount: 10)
     discount_2 = create(:discount, merchant: merchant, quantity: 12, discount: 15)
 
@@ -20,6 +20,18 @@ RSpec.describe 'merchant_discount index page' do
       expect(page).to have_content("Discount ##{discount_2.id}")
       expect(page).to have_content("Percent Discount: 15%")
       expect(page).to have_content("Quantity Threshold: 12")
+    end
+  end
+
+  it "has a link to each discount show page" do
+    merchant = create(:merchant)
+    discount_1 = create(:discount, merchant: merchant, quantity: 10, discount: 10)
+
+    visit "/merchants/#{merchant.id}/discounts"
+
+    within "div.discount_#{discount_1.id}" do
+      click_link "#{discount.name}"
+      expect(current_path).to eq(merchant_discount_path(discount_1))
     end
   end
 
